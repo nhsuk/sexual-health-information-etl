2279db085a0dd1d34a6641f0f0c04bffcc4b3f96
mark <markharrop@yahoo.com>
2018-03-27 16:51:35 +0100

    :package: use package, add retry queue

--- app/lib/azureDataService.js
+++ app/lib/azureDataService.js
@@ -1,47 +1,19 @@
-const azureService = require('./azureService');
+const AzureDataService = require('azure-data-service');
 const config = require('./config');
-const fsHelper = require('./fsHelper');
 const log = require('./logger');
 const utils = require('./utils');
-const createFilter = require('./createFileVersionFilter');
-const sortDateDesc = require('./sortByFilenameDateDesc');
-const getDateFromFilename = require('./getDateFromFilename');
 
-const outputFile = `${config.outputDir}/${config.outputFile}.json`;
-const summaryFile = `${config.outputDir}/summary.json`;
+const version = utils.getMajorMinorVersion();
+const outputDir = config.outputDir;
+const outputFile = config.outputFile;
+const containerName = config.containerName;
 
-async function getLatestDataBlob(version) {
-  return azureService.getLatestBlob(createFilter(config.outputFile, version), sortDateDesc);
-}
+const azureDataService = new AzureDataService({
+  containerName,
+  log,
+  outputDir,
+  outputFile,
+  version,
+});
 
-async function getLatestData(version) {
-  const lastScan = await getLatestDataBlob(version);
-  if (lastScan) {
-    log.info(`Latest data file '${lastScan.name}' identified`);
-    await azureService.downloadFromAzure(outputFile, lastScan.name);
-    log.info(`Latest data file '${lastScan.name}' downloaded`);
-    const data = fsHelper.loadJsonSync(config.outputFile);
-    const date = getDateFromFilename(lastScan.name);
-    return { data, date };
-  }
-  log.info(`unable to retrieve data, no data available for release ${version}?`);
-  return { data: [] };
-}
-
-function getSuffix(startMoment) {
-  return `-${startMoment.format('YYYYMMDD')}-${utils.getMajorMinorVersion()}.json`;
-}
-
-async function uploadData(startMoment) {
-  log.info(`Overwriting '${config.outputFile}' in Azure`);
-  await azureService.uploadToAzure(outputFile, `${config.outputFile}.json`);
-  log.info(`Saving date stamped version of '${config.outputFile}' in Azure`);
-  await azureService.uploadToAzure(outputFile, `${config.outputFile}${getSuffix(startMoment)}`);
-  log.info('Saving summary file in Azure');
-  await azureService.uploadToAzure(summaryFile, `${config.outputFile}-summary${getSuffix(startMoment)}`);
-}
-
-module.exports = {
-  getLatestData,
-  uploadData,
-};
+module.exports = azureDataService;
